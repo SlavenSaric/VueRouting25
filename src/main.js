e64a53121b0a3f3b33c080592a64cf042d5d0ccc
SlavenSaric <slavendownload@gmail.com>
2025-08-26 21:50:02 +0200

    routes, styleing

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+
+import App from './App.vue';
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {path: '/teams',
+    component: TeamsList,
+    },
+    {path: '/users',
+    component: UsersList,
+    },
+],
+linkActiveClass: 'active'
+});
+
+const app = createApp(App);
+app.use(router)
+
+app.mount('#app');
